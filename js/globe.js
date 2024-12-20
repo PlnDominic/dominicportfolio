cd5e390ad2508a30a3c0f8f74c97cfc67b5125d7
PlnDominic <dominickudom1738@gmail.com>
2024-12-20 12:10:29 +0000

    Update import paths for Three.js and OrbitControls from node_modules

--- js/globe.js
+++ js/globe.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import getStarfield from './src/getStarfield.js';
 import { drawThreeGeo } from './src/threeGeoJSON.js';
 
